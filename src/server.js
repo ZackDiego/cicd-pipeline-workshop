96035381170dae8e25b0d9be85014df21771cd6f
蔣永寬 <mrzackchiang@gmail.com>
2024-07-12 15:47:35 +0800

    Try the trigger event

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send("Hello, let's try auto deploy.");
+  res.send("Hello, let's try auto deploy. v1.0");
 });
 
 app.get('/db', (req, res) => {
