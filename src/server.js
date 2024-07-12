6b7bc9fc3348e9706ce7ea957ed3d2427fca051d
蔣永寬 <mrzackchiang@gmail.com>
2024-07-12 15:33:45 +0800

    Second try to test secret key works

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello this is phase 1');
+  res.send('Hello this is phase 1 second try');
 });
 
 app.get('/db', (req, res) => {
